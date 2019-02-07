022a59071d1d66466cd3ec24e276bd73be9911ab
Brendan Pettis <venture@Brendans-MacBook-Pro-2.local>
2019-02-07 16:04:45 -0600

    Fixed some bugs

--- components/src/pages/index.js
+++ components/src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import { Button } from "../components/Element/"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
@@ -13,6 +15,9 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+    </div>
+    <div>
+      <Button />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
