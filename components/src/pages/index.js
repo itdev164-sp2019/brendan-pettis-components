40d7ecbc1cd25e23edc8501355caa03a5a6bdcae
Brendan Pettis <venture@venture.attlocal.net>
2019-02-20 18:07:44 -0600

    Added Icon Button

--- components/src/pages/index.js
+++ components/src/pages/index.js
@@ -5,21 +5,22 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import { Button } from "../components/Element/"
+import { Button, IconButton, Section } from "../components/Element"
+import { Search } from 'styled-icons/feather'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>I changed something!</p>
-    <p>Now go build something great.</p>
+    <h1>Hi People</h1>
+    <p>I changed some stuff!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <div>
-      <Button />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Section flex justifyContent="space-evenly" m={2}>
+      <Link to="/page-2/">Go to page 2</Link>
+      <Button variant="primary">My Custom Button!</Button>
+      <IconButton icon={<Search />} />
+    </Section>
   </Layout>
 )
 
