34e302c05eb264a89251fca4f3ab2e32edf42178
Brendan Pettis <me@brendanpettis.com>
2019-04-04 01:03:44 -0500

    Added flow to more components

--- components/src/components/Element/Header/Header.js
+++ components/src/components/Element/Header/Header.js
@@ -1,5 +1,14 @@
+// @flow
 import React from 'react'
 import { BaseContainer } from '../BaseContainer'
+import PropTypes from 'prop-types'
 
+type Props = {
+  children: PropTypes.node
+}
 
-export const Header = props => <BaseContainer as="header" {...props} />
+export const Header = (props: Props) => <BaseContainer as="header" {...props} />
+
+Header.propTypes = {
+  children: PropTypes.node.isRequired
+}
